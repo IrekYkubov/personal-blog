1ce8ac0b55f1f97eb2ba54ff510fab8a528b6bc3
Irek Yakubov <irekyyakubov@gmail.com>
2024-06-06 21:48:14 +0400

    Правка сессий

--- modules/login/logout.php
+++ modules/login/logout.php
@@ -3,6 +3,6 @@
 unset($_SESSION['logged_user']);
 unset($_SESSION['login']);
 unset($_SESSION['role']);
-session_start();
+session_destroy();
 setcookie(session_name(), '', time() - 60, "/");
 header("location: " . HOST);
